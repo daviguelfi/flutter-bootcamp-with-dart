caf073d7ecaaf46d5e7bc1655c0a71ed3344ee95
Davi Guelfi Erba <davi.erba@luizalabs.com>
2020-09-04 14:19:05 -0300

    create a personal card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,38 +9,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purpleAccent,
+        backgroundColor: Colors.blue.shade600,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: double.infinity,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/imgs/davi.jpg'),
+              ),
+              Text(
+                'Davi Guelfi Erba',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: 'IndieFlower',
+                  color: Colors.white,
                 ),
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.green,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'PressStart2P',
+                    color: Colors.grey.shade300,
+                    fontSize: 14.0,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.grey.shade300,
+                  thickness: 2,
                 ),
-              ],
-            ),
-            Container(
-              width: 100.0,
-              height: double.infinity,
-              color: Colors.blue,
-            ),
-          ],
-        )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blue.shade600,
+                  ),
+                  title: Text('+55 16 99749 5834',
+                      style:
+                          TextStyle(color: Colors.grey.shade600, fontSize: 20)),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blue.shade600,
+                  ),
+                  title: Text('daviguelfi@gmail.com',
+                      style:
+                          TextStyle(color: Colors.grey.shade600, fontSize: 20)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
